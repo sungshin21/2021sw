6736c902ab943f54277a9ffdb74784961ba49857
elive7 <80445246+elive7@users.noreply.github.com>
2021-07-27 16:05:39 +0900

    gps 시도

--- js/main.js
+++ js/main.js
@@ -413,3 +413,21 @@ window.addEventListener("scroll", function () {
     ? ($scrollToTop.style.opacity = 1) // scrollToTop 버튼이 보이게 합니다
     : ($scrollToTop.style.opacity = 0); // 아니라면 안보이게 합니다
 });
+
+//gps
+function getLocation() {
+    if (navigator.geolocation) { // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(function(position) {
+        alert(position.coords.latitude + ' ' + position.coords.longitude);
+      }, function(error) {
+        console.error(error);
+      }, {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: Infinity
+      });
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  }
+  getLocation();
