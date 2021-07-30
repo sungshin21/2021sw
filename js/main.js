4b81e0df27c23f7cf22204fa12ebf0f2bd8f43c6
elive7 <80445246+elive7@users.noreply.github.com>
2021-07-30 22:02:59 +0900

    지도 포함 목포집

--- js/main.js
+++ js/main.js
@@ -415,19 +415,42 @@ window.addEventListener("scroll", function () {
 });
 
 //gps
-function getLocation() {
-    if (navigator.geolocation) { // GPS를 지원하면
-      navigator.geolocation.getCurrentPosition(function(position) {
-        alert(position.coords.latitude + ' ' + position.coords.longitude);
-      }, function(error) {
-        console.error(error);
-      }, {
-        enableHighAccuracy: false,
-        maximumAge: 0,
-        timeout: Infinity
-      });
-    } else {
-      alert('GPS를 지원하지 않습니다');
+// function getLocation() {
+//     if (navigator.geolocation) { // GPS를 지원하면
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//         alert(position.coords.latitude + ' ' + position.coords.longitude);
+//       }, function(error) {
+//         console.error(error);
+//       }, {
+//         enableHighAccuracy: false,
+//         maximumAge: 0,
+//         timeout: Infinity
+//       });
+//     } else {
+//       alert('GPS를 지원하지 않습니다');
+//     }
+//   }
+function showError(error) {
+    switch(error.code) {
+      case error.PERMISSION_DENIED:
+        x.innerHTML = "User denied the request for Geolocation."
+        break;
+      case error.POSITION_UNAVAILABLE:
+        x.innerHTML = "Location information is unavailable."
+        break;
+      case error.TIMEOUT:
+        x.innerHTML = "The request to get user location timed out."
+        break;
+      case error.UNKNOWN_ERROR:
+        x.innerHTML = "An unknown error occurred."
+        break;
     }
   }
-  getLocation();
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+  
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+    "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+  
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+  }
