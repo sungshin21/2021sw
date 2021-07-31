707eab205397d6b8289e11b2070a741dccea352f
elive7 <80445246+elive7@users.noreply.github.com>
2021-08-01 03:27:06 +0900

    거리 소수점 반올림

--- 서울/맛집/목포집.js
+++ 서울/맛집/목포집.js
@@ -153,7 +153,7 @@ function displayLocation(position) {
     var longitude = position.coords.longitude;
     
     var distance = computeDistance(position.coords, ourCoords);
-    $("#distance").html("당신과 목포집 사이의 대략적인 거리:"+distance+"km");
+    $("#distance").html("당신과 목포집 사이의 대략적인 거리:"+distance.toFixed(1)+"km");
                         
 }
 
