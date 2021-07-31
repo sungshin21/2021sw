ea609f82eb8d86e6fd176df5eb37ef4eb39cb408
elive7 <80445246+elive7@users.noreply.github.com>
2021-08-01 03:21:08 +0900

    누구와의 방문 클릭 이벤트 추가

--- 서울/맛집/목포집.js
+++ 서울/맛집/목포집.js
@@ -31,7 +31,75 @@ menuCloseEl.addEventListener("click", function() {
     //outside_menu 숨기기
     outsideMenuEl.style.display='none'
 })
-
+//누구와 함께하였는지 클릭할때 사용
+//가족 선택 영역자
+const withfamilyEl=document.getElementsByClassName('btn-outline-secondary')[3];
+//친구 선택 영역자
+const withfriendsEl=document.getElementsByClassName('btn-outline-secondary')[4];
+//연인 선택 영역자
+const withloverEl=document.getElementsByClassName('btn-outline-secondary')[5];
+//기타 선택 영역자
+const withothersEl=document.getElementsByClassName('btn-outline-secondary')[6];
+// 가족 선택에 클릭 이벤트가 발생하면,
+withfamilyEl.addEventListener("click", function() {
+    // 배경색 바꾸기
+    withfamilyEl.style.backgroundColor = '#0d6efd'
+    // 글씨색 바꾸기
+    withfamilyEl.style.color = '#fff'
+    //나머지 원래대로
+    withfriendsEl.style.backgroundColor = '#fff'
+    withfriendsEl.style.color = 'dimgray'
+    withloverEl.style.backgroundColor = '#fff'
+    withloverEl.style.color = 'dimgray'
+    withothersEl.style.backgroundColor = '#fff'
+    withothersEl.style.color = 'dimgray'
+    
+})
+// 친구 선택에 클릭 이벤트가 발생하면,
+withfriendsEl.addEventListener("click", function() {
+    // 배경색 바꾸기
+    withfriendsEl.style.backgroundColor = '#0d6efd'
+    // 글씨색 바꾸기
+    withfriendsEl.style.color = '#fff'
+    //나머지 원래대로
+    withfamilyEl.style.backgroundColor = '#fff'
+    withfamilyEl.style.color = 'dimgray'
+    withloverEl.style.backgroundColor = '#fff'
+    withloverEl.style.color = 'dimgray'
+    withothersEl.style.backgroundColor = '#fff'
+    withothersEl.style.color = 'dimgray'
+    
+})
+// 연인 선택에 클릭 이벤트가 발생하면,
+withloverEl.addEventListener("click", function() {
+    // 배경색 바꾸기
+    withloverEl.style.backgroundColor = '#0d6efd'
+    // 글씨색 바꾸기
+    withloverEl.style.color = '#fff'
+    //나머지 원래대로
+    withfamilyEl.style.backgroundColor = '#fff'
+    withfamilyEl.style.color = 'dimgray'
+    withfriendsEl.style.backgroundColor = '#fff'
+    withfriendsEl.style.color = 'dimgray'
+    withothersEl.style.backgroundColor = '#fff'
+    withothersEl.style.color = 'dimgray'
+    
+})
+// 기타 선택에 클릭 이벤트가 발생하면,
+withothersEl.addEventListener("click", function() {
+    // 배경색 바꾸기
+    withothersEl.style.backgroundColor = '#0d6efd'
+    // 글씨색 바꾸기
+    withothersEl.style.color = '#fff'
+    //나머지 원래대로
+    withfamilyEl.style.backgroundColor = '#fff'
+    withfamilyEl.style.color = 'dimgray'
+    withfriendsEl.style.backgroundColor = '#fff'
+    withfriendsEl.style.color = 'dimgray'
+    withloverEl.style.backgroundColor = '#fff'
+    withloverEl.style.color = 'dimgray'
+    
+})
 //별점 마킹 모듈 프로토타입으로 생성
 function Rating(){};
 Rating.prototype.rate = 0;
