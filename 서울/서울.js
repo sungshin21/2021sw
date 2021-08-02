bbc76eb2d373afb7a1700c8cfd3841a8b8dbbeae
elive7 <80445246+elive7@users.noreply.github.com>
2021-08-03 02:57:50 +0900

    서울 home에서 랜덤으로 위치 2개씩 뜨게 하기

---
+++ 서울/서울.js
@@ -0,0 +1,16 @@
+//랜덤으로 위치 2개씩 뜨게 하기 
+window.onload=function() {
+    var E = document.getElementsByClassName("card");
+    var m = E.length;
+    var n = parseInt(Math.random()*m);
+    var o = parseInt(Math.random()*m)
+    while (n == 0) {
+        var o = parseInt(Math.random()*m)
+      }
+    for (var i=m-1;i>=0;i--) {
+    var e = E[i];
+    e.style.display='none';
+    }
+    E[n].style.display='';
+    E[o].style.display='';
+    }
