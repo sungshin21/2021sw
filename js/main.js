9360e99b8dafbedf3e64078b6b60c9f1f8053d4b
elive7 <80445246+elive7@users.noreply.github.com>
2021-08-10 00:10:00 +0900

    지역별 빼고 완성...

--- js/main.js
+++ js/main.js
@@ -872,12 +872,9 @@ previous210El.addEventListener("click", function(){
 
 
 
+   // 설명창 js
 
-
-
-    // 설명창 js
-
-function Block(){
+   function Block(){
     var page = document.getElementById("page")
     page.style.display = "block";
 }
@@ -887,3 +884,82 @@ function None(){
 }
 
 
+// 코로나 확진자 순위
+
+let rollingData = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    '11',
+    '12'
+  ]    // 롤링할 데이터를 넣으면 됩니다 갯수 제한 없어요
+
+let timer = 2000 // 롤링되는 주기 입니다 (1000 => 1초)
+
+let first = document.getElementById('first'),
+second = document.getElementById('second'),
+third = document.getElementById('third')
+let move = 2
+let dataCnt = 1
+let listCnt = 1
+
+//위 선언은 따로 완전히 수정하지 않는 한 조정할 필요는 없습니다.
+
+first.children[0].innerHTML = rollingData[0]
+
+setInterval(() => {
+if(move == 2){
+first.classList.remove('card_sliding')
+first.classList.add('card_sliding_after')
+second.classList.remove('card_sliding_after')
+second.classList.add('card_sliding')
+third.classList.remove('card_sliding_after')
+third.classList.remove('card_sliding')
+
+move = 0
+} else if (move == 1){
+first.classList.remove('card_sliding_after')
+first.classList.add('card_sliding')
+
+second.classList.remove('card_sliding_after')
+second.classList.remove('card_sliding')
+
+third.classList.remove('card_sliding')
+third.classList.add('card_sliding_after')
+
+move = 2
+} else if (move == 0) {
+first.classList.remove('card_sliding_after')
+first.classList.remove('card_sliding')
+
+second.classList.remove('card_sliding')
+second.classList.add('card_sliding_after')
+
+third.classList.remove('card_sliding_after')
+third.classList.add('card_sliding')
+
+move = 1
+}
+if(dataCnt < (rollingData.length - 1)) {
+    document.getElementById('rolling_box').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+    dataCnt++
+    } else if(dataCnt == rollingData.length - 1) {
+    document.getElementById('rolling_box').children[listCnt].children[0].innerHTML = rollingData[dataCnt]
+    dataCnt = 0
+    }
+    
+    if(listCnt < 2) {
+    listCnt++
+    } else if (listCnt == 2) {
+    listCnt = 0
+    }
+    
+    console.log(listCnt)
+    }, timer);
